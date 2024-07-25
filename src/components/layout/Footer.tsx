eaeda67217f3e3fa592d6598ba2e1e0a6214fbe6
pyaythar-k <pyaythar.k@gmail.com>
2024-07-25 16:52:30 +0900

    Initial Commit

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{' '}
+          <a target="_blank" href="https://ik.com">
+            IK.
+          </a>{' '}
+        </p>
+      </small>
+    </footer>
+  );
+}
